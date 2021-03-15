e84369ad809578d0cbd4c6d9da309f12abecd206
Joao Casarin <joaovitorcasarin@hotmail.com>
2021-03-15 14:54:12 -0300

    add Access-Control-Allow headers

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, Token');
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
