f39ae67c517cd7a41006650fe266413f006032af
Joao Casarin <joaovitorcasarin@hotmail.com>
2021-03-15 16:20:11 -0300

    h1 > h3, h3 > b

--- src/controllers/Nodemailer.js
+++ src/controllers/Nodemailer.js
@@ -17,15 +17,15 @@ module.exports = class ContactMailer {
                 from: String(process.env.SENDER),
                 to: `João Vitor Casarin <${String(process.env.SENDER)}>, You <${email}>`,
                 subject: subject,
-                html: `<h1>***PLEASE, DO NOT REPLY THIS E-MAIL***</h1><br><br>
+                html: `<h3>***PLEASE, DO NOT REPLY THIS E-MAIL***</h3><br><br>
                 
-                <h3>Hey there, how are you doing? I hope everything's good!
+                <b>Hey there, how are you doing? I hope everything's good!
 
                 I have just received your message, and as soon as possible I'll be writing you back!
                 
                 Thanks!<br><br>
 
-                Your message:</h3></br>
+                Your message:</b></br>
                 
                 ------------------------------------------------------------------------------------------------------------<br><br>
                 
