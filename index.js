86431734b6a8952902b341060ac6cb24f561096b
Joao Casarin <joaovitorcasarin@hotmail.com>
2021-03-21 01:07:45 -0300

    add cors

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type']
+}));
+
+/* app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Token');
     next();
-});
+}); */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
